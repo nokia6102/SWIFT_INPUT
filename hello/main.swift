18f8565990cefa3774a1fff1c95c04da0b354213
nokia6102 <nokia6102@gmail.com>
2015-12-22 23:00:41 +0800

    Initial Commit

---
+++ hello/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hello
+//
+//  Created by Leon Cheng on 2015/12/22.
+//  Copyright (c) 2015年 Leon Cheng. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
